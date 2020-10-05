4d564631c6553b95a33e94237ea387de514b43d5
vagrant <vagrant@localhost.localdomain>
2020-10-05 09:38:58 +0000

    [add]customer_delete

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -15,14 +15,21 @@ class Public::CustomersController < ApplicationController
 	end
 
 	def delete_page
+		@customer = Customer.find(current_customer.id)
 	end
 
 	def withdraw
+		@customer = Customer.find(current_customer.id)
+		@customer.update(customer_status: '退会済')
+		#customer_statusカラムにフラグを立てる　default:false（有効会員)からtrue（退会済）へ
+		reset_session
+		#ログアウトさせる
+		redirect_to root_path
 	end
 
 	private
 	def customer_params
-		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone)
+		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone, :active)
 	end
 
 end
