a24da8b66b339a4c77484aecfb58f8471ada1c22
vagrant <vagrant@localhost.localdomain>
2020-10-06 09:29:41 +0000

    [add]passwordreset_header_top_about

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,6 +1,10 @@
 class Public::ProductsController < ApplicationController
 
 	def top
+		@product_genres = ProductGenre.all
+	end
+
+	def about
 	end
 
 	def index
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
 
     scope module: :public do
     root to: 'products#top'
+    get 'products/about'
   	end
 
   	scope module: :public do
