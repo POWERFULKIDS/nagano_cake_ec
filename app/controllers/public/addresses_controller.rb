b5c353804ceb50b275464abb06983d2e855f114e
vagrant <vagrant@localhost.localdomain>
2020-10-06 06:17:49 +0000

    (add)customer_address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,12 +1,18 @@
 class Public::AddressesController < ApplicationController
 
 	def index
+		@address = Address.new
+		@addresses = Address.where(customer_id: current_customer.id)
 	end
 
 	def edit
 	end
 
 	def create
+		@address = Address.new(address_params)
+		@address.customer = current_customer
+		@address.save
+		redirect_back(fallback_location:addresses_path)
 	end
 
 	def update
@@ -14,5 +20,10 @@ class Public::AddressesController < ApplicationController
 
 	def destroy
 	end
+
+	private
+  	def address_params
+  		params.require(:address).permit(:postcode, :address, :name)
+  	end
 
 end
