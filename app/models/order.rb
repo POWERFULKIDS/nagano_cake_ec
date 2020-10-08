057c3a11ff99d26399502cfc2461247e728b2f70
vagrant <vagrant@localhost.localdomain>
2020-10-08 14:38:45 +0000

    [add]admin_ordered_show

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,9 @@ class Order < ApplicationRecord
 	# 注文ステータス
 	enum order_status: { waiting: 0, deposited: 1, production: 2, preparation: 3, sent: 4 }
 
+	# 支払い方法
+	enum payment_method: { card: 0, transfer: 1 }
+
 	# 当日注文された件数
 	scope :created_today, -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
 
--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -1,5 +1,8 @@
 class OrderedProduct < ApplicationRecord
-	#productとorderの中間テーブル
+	# productとorderの中間テーブル
 	belongs_to :product
 	belongs_to :order
+
+	# 製作ステータス
+	enum make_status: { making: 0, makewaiting: 1, makeproduction: 2, done: 3 }
 end
